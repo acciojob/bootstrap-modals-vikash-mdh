f7f08da251c89c8378d7771735590162330708ef
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2025-02-02 11:21:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your code here
+document.getElementById("open-modal").addEventListener("click", function() {
+            var myModal = new bootstrap.Modal(document.getElementById("newton-modal"));
+            myModal.show();
+        });
